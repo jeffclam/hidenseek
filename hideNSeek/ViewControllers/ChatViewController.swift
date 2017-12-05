596df8347711942a6a63b8504ed1b464a41f9dca
CheckoutUser <checkoutuser@is-ct-a025402.local>
2017-12-04 19:41:53 -0800

    UI representation of chatmessages

--- hideNSeek/ViewControllers/ChatViewController.swift
+++ hideNSeek/ViewControllers/ChatViewController.swift
@@ -9,9 +9,33 @@
 import UIKit
 
 class ChatViewController: UIViewController {
+    var barHeight: CGFloat = 50
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var inputBar: UIView!
+    
+    override var inputAccessoryView: UIView? {
+        self.inputBar.frame.size.height = self.barHeight
+        self.inputBar.clipsToBounds = true
+        return self.inputBar
+    }
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+ 
+    func customization() {
+        self.tableView.estimatedRowHeight = self.barHeight
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        self.tableView.contentInset.bottom = self.barHeight
+        self.tableView.scrollIndicatorInsets.bottom = self.barHeight
+        self.navigationItem.title = "person"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        customization()
     }
     
     override func didReceiveMemoryWarning() {
